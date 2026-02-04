1dde7e3db4edbe0921fdf9abe016ae616a132494
Sara1502 <moreirasaragds@gmail.com>
2026-02-04 16:00:00 -0300

    Trocar fotos quando clica no botao

--- app/script.js
+++ app/script.js
@@ -1,17 +1,20 @@
-const html = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
 
+focoBt.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "foco");
+  banner.setAttribute("src", "/imagens/foco.png");
+});
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-})
+curtoBt.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "descanso-curto");
+  banner.setAttribute("src", "/imagens/descanso-curto.png");
+});
 
-curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-})
-
-longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-})
+longoBt.addEventListener("click", () => {
+  html.setAttribute("data-contexto", "descanso-longo");
+  banner.setAttribute("src", "imagens/descanso-longo.png");
+});
